8d10733b9bc01dcdd4afca3f74efc1e5ef88f8af
Pushkar Ladhe <pushkarladhe@Pushkars-MacBook-Pro.local>
2017-12-14 08:44:29 -0700

    Submitting solution for maximum subarray

---
+++ DP Solutions/src/dp/solutions/easy/leetcode/maximumsubarray/MaximumSubarray.java
@@ -0,0 +1,28 @@
+package dp.solutions.easy.leetcode.maximumsubarray;
+
+/**
+ * @author https://github.com/shinigami1392 [Pushkar Ladhe]
+ *
+ * Dynamic Programming solution for finding subarray having max sum
+ */
+
+public class MaximumSubarray {
+	public int maxSubArray(int[] nums) {
+		if (nums.length==0) {
+			return -1;
+		}
+        int currentMax = nums[0];
+        int finalMax = nums[0];
+        for (int i = 1; i <nums.length; i++){
+            currentMax = Math.max(nums[i],currentMax+nums[i]);
+            finalMax = Math.max(currentMax, finalMax);
+        }
+        return finalMax;
+    }
+	
+	/*public static void main(String[] args) {
+		MaximumSubarray ms = new MaximumSubarray();
+		int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+		System.out.println(ms.maxSubArray(nums));
+	}*/
+}
