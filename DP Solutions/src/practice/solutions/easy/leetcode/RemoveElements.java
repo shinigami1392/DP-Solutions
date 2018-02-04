5cf8a93174140c14243d490e5c38f39343a27c84
shinigami1392 <pushkar.ladhe@asu.edu>
2018-02-04 15:40:26 -0700

    Commit 16

--- DP Solutions/src/practice/solutions/easy/leetcode/RemoveElements.java
+++ DP Solutions/src/practice/solutions/easy/leetcode/RemoveElements.java
@@ -2,24 +2,15 @@ package practice.solutions.easy.leetcode;
 
 public class RemoveElements {
 	public int removeElement(int[] nums, int val) {
-        //int count = nums.length;
         if(nums.length == 0){
             return 0;
         }
-        // if (nums.length == 1){
-        //     if(nums[0] == val)
-        //         return 0;
-        //     else
-        //         return 1;
-        // }
         int i = 0;
         int pivot = nums.length;
         int j = 0;
         while (i <= pivot && i < nums.length  && pivot >=0){
             if(nums[i]==val){
-                // nums[i] = nums[pivot];
-                // nums[pivot] = 0;
-                // pivot--;
+
                 for (j = i; j <pivot-1; j++){
                     nums[j] = nums[j+1];
                 }
@@ -37,7 +28,6 @@ public class RemoveElements {
         for(int k = 0; k < pivot; k++) {
         		System.out.println(nums[k]);
         }
-        //System.out.println(pivot);
         return pivot;
     }
 	
--- DP Solutions/src/practice/solutions/easy/leetcode/TopKFrequentElements.java
+++ DP Solutions/src/practice/solutions/easy/leetcode/TopKFrequentElements.java
@@ -1,11 +1,8 @@
 package practice.solutions.easy.leetcode;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 public class TopKFrequentElements {
 	public List<Integer> topKFrequent(int[] nums, int k) {
@@ -32,23 +29,18 @@ public class TopKFrequentElements {
 	int partition(int arr[], int low, int high)
     {
         int pivot = arr[high]; 
-        int i = (low-1); // index of smaller element
+        int i = (low-1);
         for (int j=low; j<high; j++)
         {
-            // If current element is smaller than or
-            // equal to pivot
             if (arr[j] <= pivot)
             {
                 i++;
- 
-                // swap arr[i] and arr[j]
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
         }
- 
-        // swap arr[i+1] and arr[high] (or pivot)
+
         int temp = arr[i+1];
         arr[i+1] = arr[high];
         arr[high] = temp;
@@ -61,12 +53,8 @@ public class TopKFrequentElements {
     {
         if (low < high)
         {
-            /* pi is partitioning index, arr[pi] is 
-              now at right place */
             int pi = partition(arr, low, high);
- 
-            // Recursively sort elements before
-            // partition and after partition
+
             sort(arr, low, pi-1);
             sort(arr, pi+1, high);
         }
